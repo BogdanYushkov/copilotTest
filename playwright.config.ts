0cf8eba90d0d6f1a71629fe40f34b5eb798a98eb
Bogdan <bogdanyushkov8@gmail.com>
2025-08-17 17:06:46 +0300

    fix chrome erroe in the deployment

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,25 +3,38 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   timeout: 30000,
-  reporter: process.env.CI ? 
-    [
-      ['list'],
-      ['allure-playwright', {
-        detail: true,
-        outputFolder: 'allure-results',
-        suiteTitle: false
-      }],
-      ['html', { outputFolder: 'playwright-report' }],
-      ['junit', { outputFile: 'test-results/junit.xml' }]
-    ] : 
-    [
-      ['list'],
-      ['allure-playwright']
-    ],
-  use: {
-    headless: true,
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    trace: 'retain-on-failure',
-  },
+  reporter: [
+    ['list'],
+    ['allure-playwright', {
+      detail: true,
+      outputFolder: 'allure-results',
+      suiteTitle: false
+    }]
+  ],
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        browserName: 'chromium',
+        headless: true,
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+        trace: 'retain-on-failure',
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        browserName: 'firefox',
+        headless: true,
+      },
+    },
+    {
+      name: 'webkit',
+      use: {
+        browserName: 'webkit',
+        headless: true,
+      },
+    },
+  ],
 });
